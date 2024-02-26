826e63302ae52952ad7119c01c5b0a021cd9c8fd
Виталий Грахов <v.a.grahov@yandex.ru>
2024-02-26 20:18:12 +0500

    список сотрудников

--- src/main/java/ru/gravitana/lesson3/Program.java
+++ src/main/java/ru/gravitana/lesson3/Program.java
@@ -2,6 +2,17 @@ package ru.gravitana.lesson3;
 
 public class Program {
     public static void main(String[] args) {
+        Employee[] employees = {
+                new Worker("Иван", 21, 40_000),
+                new Freelancer("Дарья", 20, 300),
+                new Freelancer("Степан", 19, 300),
+                new Worker("Егор", 26, 50_000),
+                new Freelancer("Сергей", 21, 250),
+                new Worker("Анастасия", 19, 25_000),
+        };
 
+        for (Employee employee: employees) {
+            System.out.println(employee);
+        }
     }
 }
